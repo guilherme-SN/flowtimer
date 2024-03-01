540973ae4798ce198b9bbfc19cf7f2b0603b6edd
Guilherme <guilhermesouzanastoo@hotmail.com>
2024-03-01 12:16:21 -0300

    First Commit

---
+++ src/main/java/br/com/guilherme/flowtimer/FlowtimerApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.flowtimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowtimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowtimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilherme/flowtimer/FlowtimerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.flowtimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
